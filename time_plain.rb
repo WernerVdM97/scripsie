0c393d4f4091f9b95449f4a2f9409b391949e303
Werner <51920192+WernerVdM97@users.noreply.github.com>
2024-08-19 19:46:13 +0200

    carraige return

--- time_plain.rb
+++ time_plain.rb
@@ -2,11 +2,11 @@
 
 def display_time
   current_time = Time.now.strftime("%H:%M:%S")
-  puts "Current time is: #{current_time}"
+  print "\rCurrent time is: #{current_time}" # \r returns the cursor to the beginning of the line
 end
 
 while true
   display_time
-  sleep 1
+  sleep 1 # Wait for 1 second
 end
 
